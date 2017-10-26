cba29688a208effb3dbf2dae9a560a8a027cd37f
Nolan Hughes <nhughes987@gmail.com>
2017-10-26 19:59:26 +0000

    automatically backed up by learn

--- square_array.rb
+++ square_array.rb
@@ -1,13 +1,16 @@
-# def square_array(array)
-#   new_array  = []
-#   array.each do |number|
-#     new_array << number ** 2
-#   end
-#   new_array
-# end
+def square_array(array)
+  new_array  = []
+  array.each do |number|
+    new_array << number ** 2
+  end
+  new_array
+end
 
+
+array = [1, 2, 3]
 def square_array(array)
   array.collect do |number|
-    array << number ** 2
+    number ** 2
   end
 end
+
